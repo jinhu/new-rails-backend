bda00b37e9040b8fc97f008d3c752cdbccf8b792
Jinmin Hu <jinmin.hu@capgemini.com>
2025-09-29 16:27:23 +0200

    hello world with rails 9

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @post.comments.create! params.expect(comment: [:content])
+  end
+
+  private def set_post
+    @post = Post.find(params[:post_id])
+  end
+end
